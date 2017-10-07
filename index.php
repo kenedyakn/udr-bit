2e71fae6e9ba5dcbd60e27d7a0980c5b709ba2eb
kenedyivan <kenedyakenaivan@gmail.com>
2017-10-07 22:49:33 +0300

    Added header and phrase to login form

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 		<title>my git sample</title>
 	</head>
 	<body>
+		<h2>Hello world app</h2>
+		<p>Fill in form to login</p>
 		<form action="process-form.php" method="POST">
 			<input type="text" name="username" placeholder="Username">
 			<input type="password" name="password" placeholder="Password">
